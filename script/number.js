1aa51328140e3f4526dc803f9f04645420e7188c
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-10-16 23:40:16 +0100

    fixed

--- script/number.js
+++ script/number.js
@@ -1,10 +1,18 @@
 // data conversion
 let num1 = 50;
+// let twoDecimalfigures = num1.toFixed(2)
 let convertedNumberToString = String(num1);
-console.log(typeof convertedNumberToString);
+// console.log(typeof convertedNumberToString);
 
-let num2 = 40;
-let fixed = num2.toFixed(2);
-console.log(fixed);
+let num2 = 40; // number
+let fixed = num2.toFixed(2); // string
+console.log(fixed); // string
+console.log(typeof fixed); // string
 
-console.log(typeof fixed.charCodeAt());
+console.log(fixed.charCodeAt()); // number
+
+console.log(typeof fixed.charCodeAt()); // number
+
+console.log(typeof fixed); // string
+
+console.log(fixed.charCodeAt().toFixed(8).charCodeAt()); // number
