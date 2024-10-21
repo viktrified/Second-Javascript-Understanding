227a4ca05801ad53f97d2e93a347d3f2cdcc8875
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-10-21 14:23:52 +0100

    working

---
+++ script/sunday.js
@@ -0,0 +1,26 @@
+let str = "Victory";
+let num = 2;
+let bool = true;
+let n = null;
+let und = undefined;
+
+let obj = {};
+let arr = []; // array declaration 
+
+arr = [];  // 
+
+let big = 29348372938923743948327392n;
+
+// console.log(typeof obj);
+// console.log(typeof big);
+
+// if (5 === 2 + 3) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+// // console.log(export);
+
+// let firstName = "Victory";
+// let ourFirstAssignment  = ""
